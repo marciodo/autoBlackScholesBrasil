c14280213506beaf58d07d584c453f04633ec72b
marciodo <marciodo@gmail.com>
2016-04-04 07:12:55 -0300

    Adding test cases.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # autoBlackSholesBrasil
 Automate calculations with options in Brazilian stock market / Automatiza o cálculo de opções no mercado brasileiro
+
+For running test cases it will be needed the installation of nosetest and optionally rednose:
+sudo pip install nose
+sudo easyinstall rednose
---
+++ testcases/optionTest.py
@@ -0,0 +1,19 @@
+import unittest
+import datetime
+from blacksholes.option import Option
+
+""" TODO:
+    - Regular expression in option symbol
+"""
+
+class OptionTest(unittest.TestCase):
+
+    def test_createOptionWithoutPrice(self):
+	self.option = Option('abc', 12.3, datetime.date(2016, 4, 28))
+	self.assertEqual(self.option.symbol, 'abc')
+	self.assertEqual(self.option.strike, 12.3)
+	self.assertEqual(self.option.expirationDate, datetime.date(2016, 4, 28))
+	self.assertEqual(self.option.price, 0)
+	
+if __name__ == '__main__':
+    unittest.main()
